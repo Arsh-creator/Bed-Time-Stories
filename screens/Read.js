63c88d154a4a53da48468a216de61cd5cb3e6aad
Arsh-creator <75788096+Arsh-creator@users.noreply.github.com>
2021-07-30 09:16:12 +0530

    Update Read.js

--- screens/Read.js
+++ screens/Read.js
@@ -4,85 +4,13 @@ import {SearchBar, Header} from 'react-native-elements';
 import db from '../config'
 
 export default class Read extends React.Component{
-    
-    constructor(){
-     super()   
-    this.state={
-        allStories: [],
-        dataSource: [],
-        search: ''
-    }
-    }
 
-    retrieveStory=async()=>{
-        try{
-        var allStories = [];
-        var stories = db.collection("Story")
-        .get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-               allStories.push(doc.data());
-            });
-            this.setState({
-                allStories: allStories
-            })
-        })
-    }
-    catch(error) {
-            console.log("Error", error);
-        }
-    };
-
-    SearchFilterFunction (text) {
-        if (text) {
-          const newData = this.state.allStories.filter(function (item) {
-            const itemData = item.title
-              ? item.title.toUpperCase()
-              : ''.toUpperCase();
-            const textData = text.toUpperCase();
-            return itemData.indexOf(textData) > -1;
-          });
-          this.setState({
-          dataSource: newData,
-          search: text
-          })
-        }
-      }
-    
     render(){
         return(
            
             <View style={styles.container}> 
-                <Header
-                backgroundColor= {'pink'} 
-                centerComponent= {{
-                text: 'Bed Time Stories',
-                style:{color:'white', fontSize: 20}
-                }}/>
-                <View>
-                <FlatList
-          data={this.state.allStories}
-          renderItem={({item})=>(
-            <View style={{borderBottomWidth: 2}}>
-              <Text>{"Story: " + item.Story}</Text>
-              <Text>{"Title: " + item.Title}</Text>
-              <Text>{"Author: " + item.Author}</Text>
-              <Text>{"Date: " + item.date.toDate()}</Text>
-            </View>
-          )}
-          keyExtractor= {(item, index)=> index.toString()}
-          onEndReached ={this.fetchMoreTransactions}
-          onEndReachedThreshold={0.7}
-        /> 
-                </View>
-                <View style={{height: 20, width: '100%'}}>
-                    <SearchBar
-                    placeHolder = 'Type Here'
-                    onChangeText={text=> this.SearchFilterFunction(text)}
-                    onClear={text=> this.SearchFilterFunction ('')}
-                    value={this.state.search}
-                     />
-                </View>
+                <Text> A Lion lay asleep in the forest, his great head resting on his paws. A timid little Mouse came upon him unexpectedly, and in her fright and haste to get away, ran across the Lion's nose. Roused from his nap, the Lion laid his huge paw angrily on the tiny creature to kill her.
+</Text>
             </View>
         );
     }
